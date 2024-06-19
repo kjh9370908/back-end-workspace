7e85a982bdd1a48588e2aa005815ad681f89c16f
kjh9370908 <cjh9370908@gmail.com>
2024-06-19 17:47:09 +0900

    24.6.19

--- 1_MySQL/02_SELECT.sql
+++ 1_MySQL/02_SELECT.sql
@@ -328,18 +328,48 @@ LIMIT 10 OFFSET 0;
 -- 실습문제 -----
 -- 1. 직급코드(job_code)가 J7이거나 J2인 사원들 중
 --    급여(salary)가 200만원 이상인 사원들의 모든 컬럼 조회
+SELECT *
+FROM employee
+-- WHERE (job_code = 'J7' OR job_code = 'J2') AND salary >=2000000;
+WHERE job_code IN ('J7','J2') AND salary >=2000000;
+
+
 
 -- 2. 사수가 없고 부서배치도 받지 않은 사원들의 사원명(emp_name),
 --    사수비번(manager), 부서코드(dept_code) 조회
+SELECT emp_name, manager_id, dept_code
+FROM employee
+WHERE manager_id  IS NULL AND dept_code IS NULL;
 
 -- 3. 연봉(보너스포함X)이 4000만원 이상이고 보너스를 받지 않은 사원들의
 --    사번(emp_id), 사원명, 급여, 보너스(bonus) 조회 
-
+SELECT emp_id, emp_name, salary, bouns
+FROM employee
+WHERE salary * 12 >= 40000000 AND bouns IS NULL;
 -- 4. 입사일이 '1995-01-01' 이상이고 부서배치를 받은 사원들의
 --    사번, 사원명, 입사일(hire_date), 부서코드 조회 (정렬은 먼저 입사한 기준)
+SELECT emp_id, emp_name, hire_date, dept_code
+FROM employee
+WHERE hire_date >= '1995-01-01' 
+AND dept_code IS NOT NULL
+ORDER BY hire_date;
 
 -- 5. 급여가 200만원 이상 500만원 이하이고 입사일이 '2001-01-01' 이상이고
 --    보너스를 받지 않은 사원들의 사번, 사원명, 급여, 입사일, 보너스 조회 
+SELECT emp_id, emp_name, salary, hire_date, bouns
+FROM employee
+-- WHERE salary >= 2000000 AND salary <= 5000000
+WHERE salary BETWEEN 2000000 AND 5000000
+AND hire_date >= '2001-01-01'
+AND bonus IS NULL;
 
 -- 6. 보너스 포함 연봉이 NULL이 아니고 이름에 '하'가 포함되어 있는 사원들의 
--- 
+--    사번, 사원명, 급여, 보너스 포함 연봉(별칭부여) 조회 (연봉이 높은 순으로 정렬)
+--    보너스 포함 연봉 : (SALARY + SALARY * BONUS) * 12
+SELECT emp_id, emp_name, salary, (salary + salary * bouns) * 12 연봉 
+FROM employee
+WHERE (salary + salary * bouns) * 12 IS NOT NULL
+     AND emp_name LIKE '%하%'
+ORDER BY 연봉 DESC;
+
+   
